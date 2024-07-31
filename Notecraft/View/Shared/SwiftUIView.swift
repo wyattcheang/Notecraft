8141d13480e0851b014debec22dfd8574ce4da49
wyattcheang <wyattcheangwaihoe@icloud.com>
2024-07-31 23:37:40 +0800

    Feat: checkpoint submit version

---
+++ Notecraft/View/Shared/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Notecraft
+//
+//  Created by Wyatt Cheang on 21/07/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
